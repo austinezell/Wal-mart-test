c93717a60421981b11d3a75a4281591869c765e5
Austin Ezell <austin.r.ezell@gmail.com>
2016-05-17 16:43:08 -0700

    remove unused view engine in favor of direct rendering of index

--- app.js
+++ app.js
@@ -10,9 +10,6 @@ const bodyParser = require('body-parser');
 const app = express();
 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -20,14 +17,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //declares routers
-// app.get("/", (req, res) => res.render("index"));
 app.use('/content', require('./routes/content'));
 app.use('/tests', require('./routes/tests'));
 
 // render index across all routes for single-page application
-// app.all('/*', function(req, res, next) {
-//     res.render('index');
-// });
+app.all('/*', function(req, res, next) {
+  res.sendFile('index.html', { root: path.join(__dirname, "public") } );
+});
 
 const http = require('http');
 const port = constants.PORT;
